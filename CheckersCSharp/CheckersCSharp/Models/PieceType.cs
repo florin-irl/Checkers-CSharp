51323babc279165bd596ace31675e0ebb9a2cdd9
florin-irl <ivana.florin1987@gmail.com>
2024-04-03 01:09:38 +0300

    Implement EPieceType

---
+++ CheckersCSharp/CheckersCSharp/Models/PieceType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersCSharp.Models
+{
+    public enum EPieceType
+    {
+        Soldier,
+        King
+    }
+}
