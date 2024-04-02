5b8009113823c5670a7562a2d68900987c725202
florin-irl <ivana.florin1987@gmail.com>
2024-04-03 01:55:20 +0300

    Implement Board Initialization for the UI

--- CheckersCSharp/CheckersCSharp/Views/MainWindow.xaml.cs
+++ CheckersCSharp/CheckersCSharp/Views/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace CheckersCSharp
 {
     /// <summary>
@@ -20,9 +21,24 @@ namespace CheckersCSharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImages = new Image[8, 8];
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for(int r=0;r<8;r++)
+            {
+                for(int c=0;c<8;c++)
+                {
+                    Image image = new Image();
+                    pieceImages[r,c] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
         }
     }
 }
