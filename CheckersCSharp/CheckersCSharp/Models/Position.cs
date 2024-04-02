ec7660aa73678acec641462d3e6976450f649da3
florin-irl <ivana.florin1987@gmail.com>
2024-04-03 00:11:15 +0300

    Implement Direction Class + Modify Position Class

--- CheckersCSharp/CheckersCSharp/Models/Position.cs
+++ CheckersCSharp/CheckersCSharp/Models/Position.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace CheckersCSharp.Models
 {
-    internal class Position
+    public class Position
     {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column) { Row = row; Column = column; }
+
+        public EPlayer SquareColor()
+        {
+            if ((Row + Column) % 2 == 0)
+                return EPlayer.White;
+            else
+                return EPlayer.Black;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 240067226;
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Column.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
     }
 }
