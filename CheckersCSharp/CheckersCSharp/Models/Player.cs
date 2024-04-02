ea75339a08a84da7fd6b565133d76b0a0d6292d5
florin-irl <ivana.florin1987@gmail.com>
2024-04-02 23:55:41 +0300

    Implement Player Enum

---
+++ CheckersCSharp/CheckersCSharp/Models/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckersCSharp.Models
+{
+    public enum EPlayer
+    {
+        None,
+        White,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static EPlayer Opponent(this EPlayer player)
+        {
+            switch(player)
+            {
+                case EPlayer.White:
+                    return EPlayer.Black;
+                case EPlayer.Black:
+                    return EPlayer.White;
+                default:
+                    return EPlayer.None;
+            }
+        }
+    }
+}
