8a592f7e94a3cdb091f3fd29d9d07e5f4b48535a
florin-irl <ivana.florin1987@gmail.com>
2024-04-03 01:28:50 +0300

    Implement Board class

---
+++ CheckersCSharp/CheckersCSharp/Models/Board.cs
@@ -0,0 +1,34 @@
+﻿using CheckersCSharp.Models.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersCSharp.Models
+{
+    public class Board
+    {
+        private readonly Piece[,] pieces = new Piece[8, 8];
+
+        public Piece this[int row, int col]
+        {
+            get
+            {
+                return pieces[row, col];
+            }
+            set
+            {
+                pieces[row,col] = value;
+            }
+        }
+
+        public Piece this[Position pos]
+        {
+            get
+            {
+                return this[pos.Row,pos.Col];
+            }
+        }
+    }
+}
