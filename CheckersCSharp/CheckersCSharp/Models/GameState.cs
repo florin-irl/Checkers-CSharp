617393a70d8208680d4c7a6b06e96c0fa06f8da1
florin-irl <ivana.florin1987@gmail.com>
2024-04-03 09:50:02 +0300

    Update GameState.cs

--- CheckersCSharp/CheckersCSharp/Models/GameState.cs
+++ CheckersCSharp/CheckersCSharp/Models/GameState.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CheckersCSharp.Models.Moves;
+using CheckersCSharp.Models.Pieces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +13,35 @@ namespace CheckersCSharp.Models
         public Board Board { get; }
         public EPlayer CurrentPlayer { get; private set; }
 
+        public bool MultipleJumps { get; set; }
+
         public GameState(EPlayer player, Board board)
         {
             CurrentPlayer = player;
             Board = board;
+            MultipleJumps = false;
 
+        }
+
+        public IEnumerable<Move> LegalMovesForPiece(Position pos)
+        {
+            if(Board.IsEmpty(pos) || Board[pos].Color != CurrentPlayer)
+            {
+                return Enumerable.Empty<Move>();
+            }
+
+            Piece piece = Board[pos];
+            return piece.GetMoves(pos, Board);
+        }
+
+        public void MakeMove(Move move)
+        {
+            move.Execute(Board);
+            if(!MultipleJumps)
+            {
+                CurrentPlayer = CurrentPlayer.Opponent();
+            }
+            
         }
     }
 }
