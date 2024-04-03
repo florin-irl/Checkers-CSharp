4aac5f8772a3a744e1f388b113ef23e17427cb4c
florin-irl <ivana.florin1987@gmail.com>
2024-04-03 10:24:27 +0300

    Implemented UI move handling / Pieces now move on Board

--- CheckersCSharp/CheckersCSharp/Views/MainWindow.xaml.cs
+++ CheckersCSharp/CheckersCSharp/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CheckersCSharp.Models;
+using CheckersCSharp.Models.Moves;
 using CheckersCSharp.Models.Pieces;
 using CheckersCSharp.Views;
 using System;
@@ -25,8 +26,11 @@ namespace CheckersCSharp
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highlights = new Rectangle[8, 8];
+        private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
 
         private GameState gameState;
+        private Position selectedPos = null;
         public MainWindow()
         {
             InitializeComponent();
@@ -46,6 +50,10 @@ namespace CheckersCSharp
                     Image image = new Image();
                     pieceImages[r,c] = image;
                     PieceGrid.Children.Add(image);
+
+                    Rectangle highlight = new Rectangle();
+                    highlights[r, c] = highlight;
+                    HighlightGrid.Children.Add(highlight);
                 }
             }
         }
@@ -59,6 +67,91 @@ namespace CheckersCSharp
                     Piece piece = board[r, c];
                     pieceImages[r,c].Source = Images.GetImage(piece);
                 }
+            }
+        }
+
+        private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition(BoardGrid);
+            Position pos = TransformToSquarePos(point);
+
+            if(selectedPos == null)
+            {
+                OnFromPositionSelected(pos);
+            }
+            else
+            {
+                OnToPositionSelected(pos);
+            }
+
+        }
+
+        private Position TransformToSquarePos(Point point)
+        {
+            double squareSize = BoardGrid.ActualWidth / 8;
+            int row = (int)(point.Y / squareSize);
+            int col = (int)(point.X / squareSize);
+            return new Position(row, col);
+        }
+
+        private void OnFromPositionSelected(Position pos)
+        {
+            IEnumerable<Move> moves = gameState.LegalMovesForPiece(pos);
+
+            if(moves.Any())
+            {
+                selectedPos = pos;
+                CacheMoves(moves);
+                ShowHighlights();
+            }
+        }
+
+        private void OnToPositionSelected(Position pos)
+        {
+            selectedPos = null;
+            HideHighlights();
+            
+            if(moveCache.TryGetValue(pos, out Move move))
+            {
+                HandleMove(move);
+            }
+            
+          
+        }
+
+        private void HandleMove(Move move)
+        {
+            gameState.MakeMove(move);
+            DrawBoard(gameState.Board);
+        }
+
+        private void CacheMoves(IEnumerable<Move> moves)
+        {
+            moveCache.Clear();
+
+            foreach(Move move in moves)
+            {
+                moveCache[move.ToPos] = move;
+
+            }
+        }
+
+        private void ShowHighlights()
+        {
+            Color color = Color.FromArgb(150, 125, 255, 255);
+
+            foreach(Position to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = new SolidColorBrush(color);
+            }
+
+        }
+
+        private void HideHighlights()
+        {
+            foreach(Position to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = Brushes.Transparent;
             }
         }
     }
