77bfc46926008b6f0ede52592a97bebf12edff29
florin-irl <ivana.florin1987@gmail.com>
2024-04-03 09:29:00 +0300

    Update Piece.cs

--- CheckersCSharp/CheckersCSharp/Models/Pieces/Piece.cs
+++ CheckersCSharp/CheckersCSharp/Models/Pieces/Piece.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CheckersCSharp.Models.Moves;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,28 @@ namespace CheckersCSharp.Models.Pieces
         public abstract EPieceType Type { get; }
         public abstract EPlayer Color { get; }
         public abstract Piece Copy();
+
+        public abstract IEnumerable<Move> GetMoves(Position from, Board board);
+
+        protected IEnumerable<Position> MovePositionsInDir(Position from,Board board,Direction dir)
+        {
+            Position pos = from + dir;
+            if(Board.IsInside(pos) && board.IsEmpty(pos))
+            {
+                yield return pos;
+            }
+            else
+            if(Board.IsInside(pos + dir) && board.IsEmpty(pos + dir) && board[pos].Color!=Color)
+            {
+                pos = pos + dir;
+                yield return pos;
+            }
+            yield break;
+        }
+
+        protected IEnumerable<Position> MovePositionsInDirs(Position from, Board board, Direction[] dirs)
+        {
+            return dirs.SelectMany(dir => MovePositionsInDir(from, board, dir)); 
+        }
     }
 }
