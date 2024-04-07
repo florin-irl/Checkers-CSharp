4311108691d694ec3232c907a59bfa98ce801441
florin-irl <ivana.florin1987@gmail.com>
2024-04-07 11:33:23 +0300

    Implement Result class

---
+++ CheckersCSharp/CheckersCSharp/Models/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersCSharp.Models
+{
+    public class Result
+    {
+        public EPlayer Winner { get; }
+        public EEndReason Reason { get; }
+
+        public Result(EPlayer winner, EEndReason reason)
+        {
+            Winner = winner;
+            Reason = reason;
+        }
+
+        public static Result Win(EPlayer winner)
+        {
+            if(winner == EPlayer.White)
+            {
+                return new Result(EPlayer.White, EEndReason.WhiteWin);
+            }
+            else
+            {
+                return new Result(EPlayer.Black, EEndReason.BlackWin);
+            }
+        }
+    }
+}
