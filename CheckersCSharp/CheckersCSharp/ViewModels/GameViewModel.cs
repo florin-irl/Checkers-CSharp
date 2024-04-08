c8f3a2b5447824bc16ebf44172856ae69c1b071c
florin-irl <ivana.florin1987@gmail.com>
2024-04-08 13:59:03 +0300

    MAJOR COMMIT BEFORE COMMANDS INVESTORS

--- CheckersCSharp/CheckersCSharp/ViewModels/GameViewModel.cs
+++ CheckersCSharp/CheckersCSharp/ViewModels/GameViewModel.cs
@@ -1,20 +1,207 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using CheckersCSharp.Models;
+using CheckersCSharp.Models.Moves;
+using CheckersCSharp.Models.Pieces;
+using CheckersCSharp.Views;
 
 namespace CheckersCSharp.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
-        private readonly GameLogic _gameLogic;
+        private GameLogic _gameLogic;
+        public ObservableCollection<Image> PieceImages { get; set; } = new ObservableCollection<Image>();
+        public ObservableCollection<Rectangle> Highlights { get; set; } = new ObservableCollection<Rectangle>();
 
-        GameViewModel()
+        private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
+
+        private Position selectedPos = null;
+
+        public GameOverMenu GameOverMenu { get; set; } = null;
+
+        private Cursor _currentCursor;
+        public Cursor CurrentCursor
         {
+            get => _currentCursor;
+            set
+            {
+                _currentCursor = value;
+                OnPropertyChanged();
+            }
+        }
 
+        public GameViewModel()
+        {
             _gameLogic = new GameLogic(EPlayer.Black, Board.Initial());
+
+            InitializeBoard();
+
+            DrawBoard(_gameLogic.Board);
+
+            _currentCursor = _gameLogic.CurrentPlayer == EPlayer.Black ? CheckersCursors.BlackCursor : CheckersCursors.WhiteCursor;
+        }
+
+        private void InitializeBoard()
+        {
+            
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Image image = new Image();
+                    PieceImages.Add(image);
+                    //PieceGrid.Children.Add(image);
+
+                    Rectangle highlight = new Rectangle();
+                    Highlights.Add(highlight);
+                    //HighlightGrid.Children.Add(highlight);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Piece piece = board[r, c];
+                    PieceImages[r * 8 + c].Source = Images.GetImage(piece);
+                }
+            }
+        }
+        private void CacheMoves(IEnumerable<Move> moves)
+        {
+            moveCache.Clear();
+
+            foreach (var move in moves)
+            {
+                moveCache[move.ToPos] = move;
+
+            }
+        }
+
+
+
+        private void ShowHighlights()
+        {
+            var color = Color.FromArgb(150, 125, 255, 255);
+
+            foreach (var to in moveCache.Keys)
+            {
+                Highlights[to.Row * 8 + to.Column].Fill = new SolidColorBrush(color);
+            }
+
+        }
+        private void OnFromPositionSelected(Position pos)
+        {
+            IEnumerable<Move> moves = _gameLogic.LegalMovesForPiece(pos);
+
+            if (moves.Any())
+            {
+                selectedPos = pos;
+                CacheMoves(moves);
+                ShowHighlights();
+            }
+        }
+
+        private void OnToPositionSelected(Position pos)
+        {
+            selectedPos = null;
+            HideHighlights();
+
+            if (moveCache.TryGetValue(pos, out Move move))
+            {
+                if (move.Type == EMoveType.SoldierPromotion)
+                {
+                    HandlePromotion(move.FromPos, move.ToPos);
+                }
+                else
+                {
+                    HandleMove(move);
+                }
+            }
+        }
+
+
+        private void HideHighlights()
+        {
+            foreach (Position to in moveCache.Keys)
+            {
+                Highlights[to.Row * 8 + to.Column].Fill = Brushes.Transparent;
+            }
+        }
+
+        private void HandlePromotion(Position from, Position to)
+        {
+            PieceImages[to.Row * 8 + to.Column].Source = Images.GetImage(_gameLogic.CurrentPlayer, EPieceType.Soldier);
+            PieceImages[from.Row * 8 + from.Column].Source = null;
+            OnPropertyChanged(nameof(PieceImages));
+
+            Move promMove = new SoldierPromotion(from, to, EPieceType.King);
+            HandleMove(promMove);
+        }
+
+        private void HandleMove(Move move)
+        {
+            _gameLogic.MakeMove(move);
+            DrawBoard(_gameLogic.Board);
+            CurrentCursor = _gameLogic.CurrentPlayer == EPlayer.Black ? CheckersCursors.BlackCursor : CheckersCursors.WhiteCursor;
+
+            if (_gameLogic.IsGameOver())
+            {
+                ShowGameOverMenu();
+            }
+        }
+        private void ShowGameOverMenu()
+        {
+            GameOverMenu = new GameOverMenu(_gameLogic);
+            OnPropertyChanged(nameof(GameOverMenu));
+
+            GameOverMenu.OptionSelected += option =>
+            {
+                if (option == EOption.Restart)
+                {
+                    GameOverMenu.Content = null;
+                    OnPropertyChanged(nameof(GameOverMenu));
+                    RestartGame();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            };
+        }
+
+        private void RestartGame()
+        {
+            HideHighlights();
+            moveCache.Clear();
+            _gameLogic = new GameLogic(EPlayer.Black, Board.Initial());
+            DrawBoard(_gameLogic.Board);
+            CurrentCursor = _gameLogic.CurrentPlayer == EPlayer.Black ? CheckersCursors.BlackCursor : CheckersCursors.WhiteCursor;
+        }
+
+        public void OnBoardGridClicked(Position pos)
+        {
+            if (selectedPos == null)
+            {
+                OnFromPositionSelected(pos);
+            }
+            else
+            {
+                OnToPositionSelected(pos);
+            }
         }
     }
 }
--- CheckersCSharp/CheckersCSharp/Views/MainWindow.xaml.cs
+++ CheckersCSharp/CheckersCSharp/Views/MainWindow.xaml.cs
@@ -26,52 +26,14 @@ namespace CheckersCSharp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Image[,] pieceImages = new Image[8, 8];
-        private readonly Rectangle[,] highlights = new Rectangle[8, 8];
-        private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
+        
 
-        private GameViewModel _gameViewModel;
-        private Position selectedPos = null;
         public MainWindow()
         {
             InitializeComponent();
-            InitializeBoard();
-
-            _gameLogic = new GameLogic(EPlayer.Black, Board.Initial());
-
-            DrawBoard(_gameLogic.Board);
-
-            SetCursor(_gameLogic.CurrentPlayer);
         }
 
-        private void InitializeBoard()
-        {
-            for(int r=0;r<8;r++)
-            {
-                for(int c=0;c<8;c++)
-                {
-                    Image image = new Image();
-                    pieceImages[r,c] = image;
-                    PieceGrid.Children.Add(image);
-
-                    Rectangle highlight = new Rectangle();
-                    highlights[r, c] = highlight;
-                    HighlightGrid.Children.Add(highlight);
-                }
-            }
-        }
-
-        private void DrawBoard(Board board)
-        {
-            for(int r=0; r<8;r++)
-            {
-                   for(int c=0;c<8;c++)
-                {
-                    Piece piece = board[r, c];
-                    pieceImages[r,c].Source = Images.GetImage(piece);
-                }
-            }
-        }
+        
 
         private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -81,17 +43,10 @@ namespace CheckersCSharp
                 return;
             }
 
-            Point point = e.GetPosition(BoardGrid);
-            Position pos = TransformToSquarePos(point);
+            var point = e.GetPosition(BoardGrid); 
+            var pos = TransformToSquarePos(point);
 
-            if(selectedPos == null)
-            {
-                OnFromPositionSelected(pos);
-            }
-            else
-            {
-                OnToPositionSelected(pos);
-            }
+            ((GameViewModel)DataContext).OnBoardGridClicked(pos);
 
         }
 
@@ -103,132 +58,9 @@ namespace CheckersCSharp
             return new Position(row, col);
         }
 
-        private void OnFromPositionSelected(Position pos)
-        {
-            IEnumerable<Move> moves = _gameLogic.LegalMovesForPiece(pos);
-
-            if(moves.Any())
-            {
-                selectedPos = pos;
-                CacheMoves(moves);
-                ShowHighlights();
-            }
-        }
-
-        private void OnToPositionSelected(Position pos)
-        {
-            selectedPos = null;
-            HideHighlights();
-            
-            if(moveCache.TryGetValue(pos, out Move move))
-            {
-                if(move.Type==EMoveType.SoldierPromotion)
-                {
-                    HandlePromotion(move.FromPos, move.ToPos);
-                }
-                else
-                {
-                    HandleMove(move);
-                }
-            }
-            
-          
-        }
-
-        private void HandlePromotion(Position from, Position to)
-        {
-            pieceImages[to.Row,to.Column].Source = Images.GetImage(_gameLogic.CurrentPlayer,EPieceType.Soldier);
-            pieceImages[from.Row, from.Column].Source = null;
-
-            Move promMove = new SoldierPromotion(from, to, EPieceType.King);
-            HandleMove(promMove);
-        }
-
-        private void HandleMove(Move move)
-        {
-            _gameLogic.MakeMove(move);
-            DrawBoard(_gameLogic.Board);
-            SetCursor(_gameLogic.CurrentPlayer);
-
-            if(_gameLogic.IsGameOver())
-            {
-                ShowGameOverMenu();
-            }
-        }
-
-        private void CacheMoves(IEnumerable<Move> moves)
-        {
-            moveCache.Clear();
-
-            foreach(Move move in moves)
-            {
-                moveCache[move.ToPos] = move;
-
-            }
-        }
-
-        private void ShowHighlights()
-        {
-            Color color = Color.FromArgb(150, 125, 255, 255);
-
-            foreach(Position to in moveCache.Keys)
-            {
-                highlights[to.Row, to.Column].Fill = new SolidColorBrush(color);
-            }
-
-        }
-
-        private void HideHighlights()
-        {
-            foreach(Position to in moveCache.Keys)
-            {
-                highlights[to.Row, to.Column].Fill = Brushes.Transparent;
-            }
-        }
-
-        private void SetCursor(EPlayer player)
-        {
-            if(player == EPlayer.White)
-            {
-                Cursor = CheckersCursors.WhiteCursor;
-            }
-            else
-            {
-                Cursor = CheckersCursors.BlackCursor;
-            }
-        }
-
         private bool IsMenuOnScreen()
         {
             return MenuContainer.Content != null;
-        }
-
-        private void ShowGameOverMenu()
-        {
-            GameOverMenu menu = new GameOverMenu(_gameLogic);
-            MenuContainer.Content = menu;
-
-            menu.OptionSelected += option =>
-            {
-                if (option == EOption.Restart)
-                {
-                    menu.Content = null;
-                    RestartGame();
-                }
-                else
-                {
-                    Application.Current.Shutdown();
-                }
-            };
-        }
-
-        private void RestartGame()
-        {
-            HideHighlights();
-            moveCache.Clear();
-            _gameLogic = new GameLogic(EPlayer.Black, Board.Initial());
-            DrawBoard(_gameLogic.Board);
-            SetCursor(_gameLogic.CurrentPlayer);
         }
     }
 }
